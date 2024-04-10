33ee88c5cd0211a0819b3f06c11ab6f09b7df5bb
Changbaiqi <changbaiqi@foxmail.com>
2024-04-10 15:14:34 +0800

    Site updated: 2024-04-10 15:13:44

--- js/local-search.js
+++
@@ -1,5 +0,0 @@
-let path = CONFIG.root + searchPath;
-// Use CDN to accelerate the speed of loading search.xml
-if ('cdn' in CONFIG.localsearch && CONFIG.localsearch.cdn.enable === true && CONFIG.localsearch.cdn.url !== null){
-  path = CONFIG.localsearch.cdn.url;
-}
