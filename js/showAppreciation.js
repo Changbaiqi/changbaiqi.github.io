2b817b1b10fc5830ac84aabca6844a9d7cd2bf74
Changbaiqi <changbaiqi@foxmail.com>
2024-11-03 22:29:11 +0800

    Site updated: 2024-11-03 22:27:41

--- js/showAppreciation.js
+++ js/showAppreciation.js
@@ -32,6 +32,7 @@ window.onload=(function(){
         { name: 'TT',color: MCO['5'], money: '6.66', date: '2024/09/11'},
         { name: 'J*u',color: MCO['5'], money: '6.66', date: '2024/09/11'},
         { name: '*许',color: MCO['5'], money: '6.60', date: '2023/12/14'},
+        { name: 'cr迷宫吱吱鼠',color: MCO['5'], money: '5.00', date: '2024/11/03'},
         { name: '*呢',color: MCO['5'], money: '5.00', date: '2024/09/19'},
         { name: '**霖',color: MCO['5'], money: '5.00', date: '2024/09/11'},
         { name: '行风行里',color: MCO['5'], money: '5.00', date: '2024/05/09'},
